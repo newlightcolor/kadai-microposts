6e9c321db82c4e27188cded363264b3cd63ccca3
yuta.noritake <newlightcolor@gmail.com>
2019-11-16 14:00:05 +0000

    timeline

--- resources/views/users/followers.blade.php
+++ resources/views/users/followers.blade.php
@@ -6,6 +6,7 @@
             @include('users.card', ['user' => $user])
         </aside>
         <div class="col-sm-8">
+            
             @include('users.navtabs', ['user' => $user])
             @include('users.users', ['users' => $users])
         </div>
